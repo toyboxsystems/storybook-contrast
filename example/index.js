9aa4de4f0db4f778882bedf86f8325c11ff04bf5
Jono Kolnik <jono@toyboxsystems.com>
2020-10-20 15:06:08 -0400

    cleanup

--- example/index.js
+++ example/index.js
@@ -1,4 +1,18 @@
 import * as React from "react";
 import { storiesOf, setAddon } from "@storybook/react";
 
-storiesOf("Example", module).add("Static source", () => <div />);
+storiesOf("Example", module)
+    .add("Static source", () => (
+        <div>
+            <span>
+                <p>hello, world.</p>
+            </span>
+        </div>
+    ))
+    .add("foo", () => (
+        <div>
+            <span>
+                <p>foo, bar.</p>
+            </span>
+        </div>
+    ));
