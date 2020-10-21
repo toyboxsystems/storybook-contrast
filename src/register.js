d1eebbc98571797a823e7e50140be4fd5fd1c232
Jono Kolnik <jono@toyboxsystems.com>
2020-10-21 13:30:41 -0400

    we can get source of story if we decorate with source loader in preset

--- src/register.js
+++ src/register.js
@@ -1,19 +1,23 @@
 import React from "react";
+import _ from "lodash";
 import { addons, types } from "@storybook/addons";
 import { AddonPanel } from "@storybook/components";
 import { useStorybookState } from "@storybook/api";
-import { StoryPanel } from "./StoryPanel";
 
-const ADDON_ID = "myaddon";
+const ADDON_ID = "contrast-app";
 const PANEL_ID = `${ADDON_ID}/panel`;
 
 const SRC = "http://localhost:3000";
 
+window.linkedContrast = false;
+
 const sendMessage = json => {
     // Make sure you are sending a string, and to stringify JSON
-    document
-        .getElementById("the_iframe")
-        .contentWindow.postMessage(JSON.stringify(json), "*");
+
+    const frame = document.getElementById("the_iframe");
+    if (frame && frame.contentWindow) {
+        frame.contentWindow.postMessage(JSON.stringify(json), "*");
+    }
 };
 
 const sendState = () => {
@@ -23,57 +27,60 @@ const sendState = () => {
     });
 };
 
-const setStorySource = source => {
-    window.contrastStorySource = source;
+const sendStory = () => {
+    const state = useStorybookState();
+    const story = state["storiesHash"][state["storyId"]];
+    if (story && story["parameters"]) {
+        const parameters = story["parameters"];
+        sendMessage({
+            type: "storybook_source",
+            data: {
+                name: story["name"],
+                url: window.location.href,
+                urlPath: story["path"],
+                codeSnippet: parameters["storySource"]["source"],
+                fileName: parameters["fileName"]
+            }
+        });
+    }
 };
 
-const sendStorySource = () => {
-    sendMessage({
-        type: "storybook_source",
-        data: window.contrastStorybookState
-    });
+const setStorySource = source => {
+    window.contrastStorySource = source;
+    sendStory();
 };
 
 const setup = () => {
-    console.log("SETUP");
+    if (!window.linkedContrast) {
+        console.log("linked contrast");
+        window.linkedContrast = true;
 
-    window.setupContrast = true;
-
-    // addEventListener support for IE8
-    function bindEvent(element, eventName, eventHandler) {
-        if (element.addEventListener) {
-            element.addEventListener(eventName, eventHandler, false);
-        } else if (element.attachEvent) {
-            element.attachEvent("on" + eventName, eventHandler);
-        }
-    }
-
-    // Listen to message from child window
-    bindEvent(window, "message", function (e) {
-        console.log("from child", e);
-        if (e.origin === SRC) {
-            const json = JSON.parse(e.data);
-
-            switch (json.type) {
-                case "get_state":
-                    return sendState();
+        // addEventListener support for IE8
+        function bindEvent(element, eventName, eventHandler) {
+            if (element.addEventListener) {
+                element.addEventListener(eventName, eventHandler, false);
+            } else if (element.attachEvent) {
+                element.attachEvent("on" + eventName, eventHandler);
             }
         }
-    });
 
-    sendState();
+        // Listen to message from child window
+        bindEvent(window, "message", function (e) {
+            if (e.origin === SRC) {
+                const json = JSON.parse(e.data);
+
+                switch (json.type) {
+                    case "get_story":
+                        return sendStory();
+                    case "get_state":
+                        return sendState();
+                }
+            }
+        });
+    }
 };
 
 const Content = () => {
-    const state = useStorybookState();
-    window.contrastStorybookState = state;
-    console.log("SETTING STATE");
-
-    if (window.setupContrast) {
-        sendState();
-    }
-
-    // return null;
     return (
         <iframe
             onLoad={setup}
@@ -90,15 +97,10 @@ addons.register(ADDON_ID, api => {
         type: types.PANEL,
         title: "Contrast",
         render: ({ active, key }) => {
+            sendStory();
+
             return (
                 <AddonPanel active={active} key={key}>
-                    <span style={{ display: "none" }}>
-                        <StoryPanel
-                            key={key}
-                            api={api}
-                            setSource={setStorySource}
-                        />
-                    </span>
                     <Content />
                 </AddonPanel>
             );
